0c853c1a6dd7bad6341036954481f5e7831a0ab1
Prabesh <prabesh.bista369@gmail.com>
2022-05-15 10:45:40 +0545

    Sending Data from View to Controller

--- src/main/java/controller/FormController.java
+++ src/main/java/controller/FormController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class FormController {
+    @RequestMapping("/")
+    public  String form(){
+        return "form";
+    }
 }
